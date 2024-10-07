15a9796074baa6d14db285f814ddd7d0468e880b
tranlam181 <tranlam181@gmail.com>
2024-10-07 14:07:49 +0700

    Create comp.umd.js

---
+++ comp.umd.js
@@ -0,0 +1,14 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global = global || self, global.umdModule = factory());
+}(this, function () {
+    function renderReactComponent() {
+        const element = React.createElement('h1', null, 'Hello from UMD.js React Component');
+        ReactDOM.render(element, document.getElementById('root'));
+    }
+
+    return {
+        renderReactComponent: renderReactComponent
+    };
+}));
