f4f5df817b622164ebd902fd82f1045f87c30582
tranlam181 <tranlam181@gmail.com>
2024-10-08 09:42:38 +0700

    Update comp.umd.js

--- comp.umd.js
+++ comp.umd.js
@@ -3,9 +3,9 @@
     typeof define === 'function' && define.amd ? define(factory) :
     (global = global || self, global.umdModule = factory());
 }(this, function () {
-    function renderReactComponent(React, ReactDOM) {
-        const element = React.createElement('h1', null, 'Hello from UMD.js React Component');
-        ReactDOM.render(element, document.getElementById('root'));
+    function renderReactComponent(createElement, render) {
+        const element = createElement('h1', null, 'Hello from UMD.js React Component');
+        render(element, document.getElementById('root'));
     }
 
     return {
